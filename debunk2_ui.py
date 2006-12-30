234fa414df0eb522bfcc67bb84b650f9159308ff
havard.dahle <havard.dahle@abffd066-d225-0410-88e5-87db5789ed7d>
2006-12-30 14:09:48 +0000

    Updating generated gui file to qt4

--- debunk2_ui.py
+++ debunk2_ui.py
@@ -2,97 +2,97 @@
 
 # Form implementation generated from reading ui file 'debunk2.ui'
 #
-# Created: Fri Dec 29 17:58:32 2006
-#      by: The PyQt User Interface Compiler (pyuic) 3.16
+# Created: Sat Dec 30 15:08:54 2006
+#      by: PyQt4 UI code generator 4.0.1
 #
 # WARNING! All changes made in this file will be lost!
 
-
 import sys
-from qt import *
-from qttable import QTable
+from PyQt4 import QtCore, QtGui
 
+class Ui_debunk2(object):
+    def setupUi(self, debunk2):
+        debunk2.setObjectName("debunk2")
+        debunk2.resize(QtCore.QSize(QtCore.QRect(0,0,592,470).size()).expandedTo(debunk2.minimumSizeHint()))
+        debunk2.setSizeGripEnabled(True)
 
-class debunk2(QDialog):
-    def __init__(self,parent = None,name = None,modal = 0,fl = 0):
-        QDialog.__init__(self,parent,name,modal,fl)
+        self.Layout5 = QtGui.QWidget(debunk2)
+        self.Layout5.setGeometry(QtCore.QRect(500,30,82,410))
+        self.Layout5.setObjectName("Layout5")
 
-        if not name:
-            self.setName("debunk2")
+        self.vboxlayout = QtGui.QVBoxLayout(self.Layout5)
+        self.vboxlayout.setMargin(0)
+        self.vboxlayout.setSpacing(6)
+        self.vboxlayout.setObjectName("vboxlayout")
 
-        self.setSizeGripEnabled(1)
+        self.buttonOk = QtGui.QPushButton(self.Layout5)
+        self.buttonOk.setAutoDefault(True)
+        self.buttonOk.setDefault(True)
+        self.buttonOk.setObjectName("buttonOk")
+        self.vboxlayout.addWidget(self.buttonOk)
 
+        self.buttonCancel = QtGui.QPushButton(self.Layout5)
+        self.buttonCancel.setAutoDefault(True)
+        self.buttonCancel.setObjectName("buttonCancel")
+        self.vboxlayout.addWidget(self.buttonCancel)
 
-        LayoutWidget = QWidget(self,"Layout5")
-        LayoutWidget.setGeometry(QRect(500,30,82,410))
-        Layout5 = QVBoxLayout(LayoutWidget,0,6,"Layout5")
+        spacerItem = QtGui.QSpacerItem(20,20,QtGui.QSizePolicy.Minimum,QtGui.QSizePolicy.Expanding)
+        self.vboxlayout.addItem(spacerItem)
 
-        self.buttonOk = QPushButton(LayoutWidget,"buttonOk")
-        self.buttonOk.setAutoDefault(1)
-        self.buttonOk.setDefault(1)
-        Layout5.addWidget(self.buttonOk)
+        self.nk2Location = QtGui.QComboBox(debunk2)
+        self.nk2Location.setGeometry(QtCore.QRect(10,30,371,21))
+        self.nk2Location.setAcceptDrops(True)
+        self.nk2Location.setEditable(True)
+        self.nk2Location.setAutoCompletion(True)
+        self.nk2Location.setDuplicatesEnabled(False)
+        self.nk2Location.setObjectName("nk2Location")
 
-        self.buttonCancel = QPushButton(LayoutWidget,"buttonCancel")
-        self.buttonCancel.setAutoDefault(1)
-        Layout5.addWidget(self.buttonCancel)
-        Spacer1 = QSpacerItem(20,20,QSizePolicy.Minimum,QSizePolicy.Expanding)
-        Layout5.addItem(Spacer1)
+        self.nk2Locator = QtGui.QPushButton(debunk2)
+        self.nk2Locator.setGeometry(QtCore.QRect(390,30,94,21))
+        self.nk2Locator.setObjectName("nk2Locator")
 
-        self.nk2Locator = QPushButton(self,"nk2Locator")
-        self.nk2Locator.setGeometry(QRect(390,30,94,21))
+        self.parsedTable = QtGui.QTableWidget(debunk2)
+        self.parsedTable.setGeometry(QtCore.QRect(10,60,481,381))
+        self.parsedTable.setDragEnabled(True)
+        self.parsedTable.setAlternatingRowColors(True)
+        self.parsedTable.setObjectName("parsedTable")
 
-        self.nk2Location = QComboBox(0,self,"nk2Location")
-        self.nk2Location.setGeometry(QRect(10,30,371,21))
-        self.nk2Location.setAcceptDrops(1)
-        self.nk2Location.setEditable(1)
-        self.nk2Location.setAutoCompletion(1)
-        self.nk2Location.setDuplicatesEnabled(0)
+        self.retranslateUi(debunk2)
+        QtCore.QObject.connect(self.buttonOk,QtCore.SIGNAL("clicked()"),debunk2.accept)
+        QtCore.QObject.connect(self.buttonCancel,QtCore.SIGNAL("clicked()"),debunk2.reject)
+        QtCore.QMetaObject.connectSlotsByName(debunk2)
+        debunk2.setTabOrder(self.nk2Location,self.nk2Locator)
+        debunk2.setTabOrder(self.nk2Locator,self.parsedTable)
+        debunk2.setTabOrder(self.parsedTable,self.buttonOk)
+        debunk2.setTabOrder(self.buttonOk,self.buttonCancel)
 
-        self.parsedTable = QTable(self,"parsedTable")
-        self.parsedTable.setNumCols(self.parsedTable.numCols() + 1)
-        self.parsedTable.horizontalHeader().setLabel(self.parsedTable.numCols() - 1,self.__tr("Name"))
-        self.parsedTable.setNumCols(self.parsedTable.numCols() + 1)
-        self.parsedTable.horizontalHeader().setLabel(self.parsedTable.numCols() - 1,self.__tr("Address"))
-        self.parsedTable.setNumRows(self.parsedTable.numRows() + 1)
-        self.parsedTable.verticalHeader().setLabel(self.parsedTable.numRows() - 1,self.__tr("1"))
-        self.parsedTable.setGeometry(QRect(10,70,480,370))
-        self.parsedTable.setResizePolicy(QTable.Default)
-        self.parsedTable.setVScrollBarMode(QTable.AlwaysOn)
-        self.parsedTable.setDragAutoScroll(1)
-        self.parsedTable.setNumRows(1)
-        self.parsedTable.setNumCols(2)
-        self.parsedTable.setReadOnly(0)
+    def retranslateUi(self, debunk2):
+        debunk2.setWindowTitle(QtGui.QApplication.translate("debunk2", "debuNK2", None, QtGui.QApplication.UnicodeUTF8))
+        self.buttonOk.setText(QtGui.QApplication.translate("debunk2", "&OK", None, QtGui.QApplication.UnicodeUTF8))
+        self.buttonCancel.setText(QtGui.QApplication.translate("debunk2", "&Cancel", None, QtGui.QApplication.UnicodeUTF8))
+        self.nk2Location.addItem(QtGui.QApplication.translate("debunk2", "Trying to find autocompletion (NK2) files...", None, QtGui.QApplication.UnicodeUTF8))
+        self.nk2Locator.setText(QtGui.QApplication.translate("debunk2", "Locate NK2...", None, QtGui.QApplication.UnicodeUTF8))
+        self.parsedTable.clear()
+        self.parsedTable.setColumnCount(2)
+        self.parsedTable.setRowCount(1)
 
-        self.languageChange()
+        headerItem = QtGui.QTableWidgetItem()
+        headerItem.setText(QtGui.QApplication.translate("debunk2", "1", None, QtGui.QApplication.UnicodeUTF8))
+        self.parsedTable.setVerticalHeaderItem(0,headerItem)
 
-        self.resize(QSize(592,470).expandedTo(self.minimumSizeHint()))
-        self.clearWState(Qt.WState_Polished)
+        headerItem1 = QtGui.QTableWidgetItem()
+        headerItem1.setText(QtGui.QApplication.translate("debunk2", "Name", None, QtGui.QApplication.UnicodeUTF8))
+        self.parsedTable.setHorizontalHeaderItem(0,headerItem1)
 
-        self.connect(self.buttonOk,SIGNAL("clicked()"),self.accept)
-        self.connect(self.buttonCancel,SIGNAL("clicked()"),self.reject)
+        headerItem2 = QtGui.QTableWidgetItem()
+        headerItem2.setText(QtGui.QApplication.translate("debunk2", "Address", None, QtGui.QApplication.UnicodeUTF8))
+        self.parsedTable.setHorizontalHeaderItem(1,headerItem2)
 
-
-    def languageChange(self):
-        self.setCaption(self.__tr("debuNK2"))
-        self.buttonOk.setText(self.__tr("&OK"))
-        self.buttonOk.setAccel(QKeySequence(QString.null))
-        self.buttonCancel.setText(self.__tr("&Cancel"))
-        self.buttonCancel.setAccel(QKeySequence(QString.null))
-        self.nk2Locator.setText(self.__tr("Locate NK2..."))
-        self.nk2Location.clear()
-        self.nk2Location.insertItem(self.__tr("Trying to find autocompletion (NK2) files..."))
-        self.parsedTable.horizontalHeader().setLabel(0,self.__tr("Name"))
-        self.parsedTable.horizontalHeader().setLabel(1,self.__tr("Address"))
-        self.parsedTable.verticalHeader().setLabel(0,self.__tr("1"))
-
-
-    def __tr(self,s,c = None):
-        return qApp.translate("debunk2",s,c)
 
 if __name__ == "__main__":
-    a = QApplication(sys.argv)
-    QObject.connect(a,SIGNAL("lastWindowClosed()"),a,SLOT("quit()"))
-    w = debunk2()
-    a.setMainWidget(w)
-    w.show()
-    a.exec_loop()
+    app = QtGui.QApplication(sys.argv)
+    debunk2 = QtGui.QDialog()
+    ui = Ui_debunk2()
+    ui.setupUi(debunk2)
+    debunk2.show()
+    sys.exit(app.exec_())
