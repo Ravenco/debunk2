d47d4f0c6936722928bb96797184b02ad1de0ad4
havard.dahle <havard.dahle@abffd066-d225-0410-88e5-87db5789ed7d>
2007-01-01 18:59:14 +0000

    Adding files for distribution

---
+++ setup.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+# -*- coding:utf8 -*-
+###########################################################################
+#    Copyright (C) 2007 - Håvard Dahle 
+#    <havard@dahle.no>
+#
+#    Lisens: GPL2
+#
+# $Id: setup.py 24 2006-10-06 20:30:04Z havard.dahle $
+###########################################################################
+
+
+from distutils.core import setup
+#from setuptools import setup
+
+import sys, os.path
+
+
+setup(name="debunk2",
+      version=0.1,
+      description="Read MS Outlook autocomplete (NK2) files and extract email addresses",
+      author='Håvard Dahle',
+      author_email="havard@dahle.no",
+      url="http://code.google.com/p/debunk2/",
+      #packages=['finfaktura',],
+      data_files=[#('share/finfaktura/pixmaps', ['pixmaps/error.png', 'pixmaps/warning.png']),
+            ('share/debunk2/data', ['debunk2.ui', ]),
+           ],
+      scripts=["debunk2.py"],
+      license="GPL2",
+      #long_description="""Microsoft Outlook stores its autocomplete email info in an undocumented file format. This project tries to unlock the information therein.""",
+      #install_requires = ['docutils>=0.3', 'reportlab'],
+      #zip_safe=True,
+     )
