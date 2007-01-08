01fff9e85a1b3b59fa5a863b25194d1388b89ba4
havard.dahle <havard.dahle@abffd066-d225-0410-88e5-87db5789ed7d>
2007-01-08 20:06:40 +0000

    removing unneeded stuff

--- vars.bat
+++ vars.bat
@@ -1,22 +1,18 @@
 @echo off
 rem
-rem This file is generated
-rem
-
-rem python setup.py py2exe --includes sip
+rem $Id$
+rem <havard@dahle.no>
 
 echo Setting up a MinGW/Qt only environment...
 echo -- QTDIR set to C:\Qt
 echo -- PATH set to C:\Qt\bin
 echo -- Adding C:\qt\MinGW\bin to PATH
 echo -- Adding %SystemRoot%\System32 to PATH
-echo -- QMAKESPEC set to win32-g++
 
 set QTDIR=C:\Qt
 set PATH=C:\Qt\bin
 set PATH=%PATH%;C:\qt\MinGW\bin
 set PATH=%PATH%;%USERPROFILE%\prog\python25
 set PATH=%PATH%;%SystemRoot%\System32
-set QMAKESPEC=win32-g++
 
 
